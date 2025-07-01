172cebff0a2c5821bc092728611b6790cab33807
Adarsh Kumar <adarshku123456789@gmail.com>
2025-07-01 15:31:26 +0530

    getAccounts

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,5 +1,7 @@
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+
+import accounts  from './accounts'
 import { Hono } from 'hono'
+import { HTTPException } from 'hono/http-exception'
 import { handle } from 'hono/vercel'
 
 
@@ -7,26 +9,20 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app
-  .get(
-    '/hello',
-    clerkMiddleware(),
-    (c) => {
-      const auth = getAuth(c)
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse()
+  }
 
-      if (!auth?.userId) {
-        return c.json({
-          error: "Unauthorzied"
-        })
-      }
-
-
-      return c.json({
-        message: 'Hello Next.js!',
-        userId: auth.userId
-      })
+  return c.json({error: "Internal error..."}, 500)
 })
+
+const routes = app
+  .route("/accounts", accounts)
 
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+
+export type AppType = typeof routes;
