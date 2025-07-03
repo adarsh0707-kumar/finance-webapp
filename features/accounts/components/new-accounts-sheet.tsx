0249aff38660965dd52627e18a547b58b1c7be11
Adarsh Kumar <adarshku123456789@gmail.com>
2025-07-03 13:48:03 +0530

    Add Toaster component and integrate account creation form with API

--- features/accounts/components/new-accounts-sheet.tsx
+++ features/accounts/components/new-accounts-sheet.tsx
@@ -6,10 +6,32 @@ import {
   SheetTitle
 } from "@/components/ui/sheet"
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account"
+import { AccountForm } from "@/features/accounts/components/account-form";
+import { insertAccountSchema } from "@/db/schema";
+import { z } from "zod";
+import { useCreateAccount } from "@/features/accounts/api/use-create-account";
+
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+})
+
+type FormValues = z.input<typeof formSchema>
+
 
 export const NewAccountsSheet = () => {
   
   const { isOpen, onClose } = useNewAccount();
+
+  const mutation = useCreateAccount()
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose()
+      }
+    })
+  }
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
@@ -22,6 +44,13 @@ export const NewAccountsSheet = () => {
             Create a new accounts to track your transactions.
           </SheetDescription>
         </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{
+            name: "",
+          }}
+        />
 
       </SheetContent>
     </Sheet>
